74b418038d993bc82823f9321c9c5131be9403b4
your_username <your_email@gmail.com>
2022-07-18 14:10:25 +0200

    Begin to add coordinate to TreeModel

--- lib/domain/entities/tree.dart
+++ lib/domain/entities/tree.dart
@@ -1,15 +1,19 @@
-class Tree {
+import 'package:google_maps_cluster_manager/google_maps_cluster_manager.dart';
+import 'package:google_maps_flutter_platform_interface/src/types/location.dart';
+
+class Tree with ClusterItem {
   final int id;
   final String? name;
+  final LatLng coordinate;
 
-  Tree({required this.id, this.name});
+  Tree({required this.id, this.name, required this.coordinate});
 
   Map<String, dynamic> toJson() {
     return {'id': id, 'name': name};
   }
+
+  @override
+  LatLng get location => coordinate;
 }
 
-final initialTree = Tree(
-  id: 0,
-  name: '',
-);
+final initialTree = Tree(id: 0, name: '', coordinate: const LatLng(0.0, 0.0));
