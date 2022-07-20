0699adfbab1554180a5ac66c07a5c7284d7fb2b3
your_username <your_email@gmail.com>
2022-07-20 10:05:45 +0200

    Fix markers not appear after moving logical to ClusterMapState

--- lib/presentation/map/cluster_map_screen.dart
+++ lib/presentation/map/cluster_map_screen.dart
@@ -1,15 +1,15 @@
 import 'dart:async';
 import 'dart:ui';
-import 'package:clean_archi_flutter_tree_list/domain/entities/tree.dart';
+
+import 'package:clean_archi_flutter_tree_list/presentation/main/tree_getter_state.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_maps_cluster_manager/google_maps_cluster_manager.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
+import '../../domain/entities/tree.dart';
 import '../../injector.dart';
-import '../main/tree_getter_state.dart';
-// import './cluster_map_state.dart';
 
 class ClusterMapScreen extends StatefulWidget {
   const ClusterMapScreen({Key? key}) : super(key: key);
@@ -21,30 +21,48 @@ class ClusterMapScreen extends StatefulWidget {
 class ClusterMapScreenState extends State<ClusterMapScreen> {
   late ClusterManager _manager;
 
-  TreeGetterState treegetterState = getIt.get<TreeGetterState>();
-  // ClusterMapState clusterMapState = getIt.get<ClusterMapState>();
+  TreeGetterState treeListState = getIt.get<TreeGetterState>();
 
   final Completer<GoogleMapController> _controller = Completer();
+
+  // ignore: prefer_collection_literals
+  Set<Marker> markers = Set();
 
   final CameraPosition _parisCameraPosition =
       const CameraPosition(target: LatLng(48.856613, 2.352222), zoom: 12.0);
 
-  Set<Marker> markers = Set();
-
   @override
   void initState() {
-    _manager = initClusterManager();
+    _manager = _initClusterManager();
     super.initState();
   }
 
-  ClusterManager initClusterManager() {
-    return ClusterManager<Tree>(treegetterState.trees, _updateMarkers,
+  ClusterManager _initClusterManager() {
+    return ClusterManager<Tree>(treeListState.trees, _updateMarkers,
         markerBuilder: _markerBuilder);
   }
 
-  void _updateMarkers(Set<Marker> maloadedMarkers) {
+  void _updateMarkers(Set<Marker> markers) {
     debugPrint('Updated ${markers.length} markers');
-    markers = maloadedMarkers;
+    setState(() {
+      this.markers = markers;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GoogleMap(
+          mapType: MapType.normal,
+          initialCameraPosition: _parisCameraPosition,
+          markers: markers,
+          onMapCreated: (GoogleMapController controller) {
+            _controller.complete(controller);
+            _manager.setMapId(controller.mapId);
+          },
+          onCameraMove: _manager.onCameraMove,
+          onCameraIdle: _manager.updateMap),
+    );
   }
 
   Future<Marker> Function(Cluster<Tree>) get _markerBuilder => (cluster) async {
@@ -95,69 +113,4 @@ class ClusterMapScreenState extends State<ClusterMapScreen> {
 
     return BitmapDescriptor.fromBytes(data.buffer.asUint8List());
   }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: GoogleMap(
-          mapType: MapType.normal,
-          initialCameraPosition: _parisCameraPosition,
-          markers: markers,
-          onMapCreated: (GoogleMapController controller) {
-            _controller.complete(controller);
-            _manager.setMapId(controller.mapId);
-          },
-          onCameraMove: _manager.onCameraMove,
-          onCameraIdle: _manager.updateMap),
-    );
-  }
-
-  // Future<Marker> Function(Cluster<Tree>) get _markerBuilder => (cluster) async {
-  //       return Marker(
-  //         markerId: MarkerId(cluster.getId()),
-  //         position: cluster.location,
-  //         onTap: () {
-  //           debugPrint('---- $cluster');
-  //           for (var p in cluster.items) {
-  //             debugPrint('$p');
-  //           }
-  //         },
-  //         icon: await _getMarkerBitmap(cluster.isMultiple ? 125 : 75,
-  //             text: cluster.isMultiple ? cluster.count.toString() : null),
-  //       );
-  //     };
-
-  // Future<BitmapDescriptor> _getMarkerBitmap(int size, {String? text}) async {
-  //   if (kIsWeb) size = (size / 2).floor();
-
-  //   final PictureRecorder pictureRecorder = PictureRecorder();
-  //   final Canvas canvas = Canvas(pictureRecorder);
-  //   final Paint paint1 = Paint()..color = Colors.orange;
-  //   final Paint paint2 = Paint()..color = Colors.white;
-
-  //   canvas.drawCircle(Offset(size / 2, size / 2), size / 2.0, paint1);
-  //   canvas.drawCircle(Offset(size / 2, size / 2), size / 2.2, paint2);
-  //   canvas.drawCircle(Offset(size / 2, size / 2), size / 2.8, paint1);
-
-  //   if (text != null) {
-  //     TextPainter painter = TextPainter(textDirection: TextDirection.ltr);
-  //     painter.text = TextSpan(
-  //       text: text,
-  //       style: TextStyle(
-  //           fontSize: size / 3,
-  //           color: Colors.white,
-  //           fontWeight: FontWeight.normal),
-  //     );
-  //     painter.layout();
-  //     painter.paint(
-  //       canvas,
-  //       Offset(size / 2 - painter.width / 2, size / 2 - painter.height / 2),
-  //     );
-  //   }
-
-  //   final img = await pictureRecorder.endRecording().toImage(size, size);
-  //   final data = await img.toByteData(format: ImageByteFormat.png) as ByteData;
-
-  //   return BitmapDescriptor.fromBytes(data.buffer.asUint8List());
-  // }
 }
