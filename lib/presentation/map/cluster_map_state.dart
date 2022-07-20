1c9da13f85a90bf10793465a9389d82ff7018c56
your_username <your_email@gmail.com>
2022-07-20 09:58:31 +0200

    Move GoogleMaps APIKey for Android

--- lib/presentation/map/cluster_map_state.dart
+++ lib/presentation/map/cluster_map_state.dart
@@ -1,5 +1,15 @@
+import 'dart:ui';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_maps_cluster_manager/google_maps_cluster_manager.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:mobx/mobx.dart';
+
+import '../../domain/entities/tree.dart';
+import '../../injector.dart';
+import '../main/tree_getter_state.dart';
 
 part 'cluster_map_state.g.dart';
 
@@ -8,7 +18,69 @@ class ClusterMapState = ClusterMapStateBase with _$ClusterMapState;
 abstract class ClusterMapStateBase with Store {
   ClusterMapStateBase();
 
+  TreeGetterState treegetterState = getIt.get<TreeGetterState>();
+
   @observable
   // ignore: prefer_collection_literals
   Set<Marker> markers = Set();
+
+  @action
+  ClusterManager initClusterManager() {
+    return ClusterManager<Tree>(treegetterState.trees, _updateMarkers,
+        markerBuilder: _markerBuilder);
+  }
+
+  void _updateMarkers(Set<Marker> maloadedMarkers) {
+    debugPrint('Updated ${markers.length} markers');
+    markers = maloadedMarkers;
+  }
+
+  Future<Marker> Function(Cluster<Tree>) get _markerBuilder => (cluster) async {
+        return Marker(
+          markerId: MarkerId(cluster.getId()),
+          position: cluster.location,
+          onTap: () {
+            debugPrint('---- $cluster');
+            for (var p in cluster.items) {
+              debugPrint('$p');
+            }
+          },
+          icon: await _getMarkerBitmap(cluster.isMultiple ? 125 : 75,
+              text: cluster.isMultiple ? cluster.count.toString() : null),
+        );
+      };
+
+  Future<BitmapDescriptor> _getMarkerBitmap(int size, {String? text}) async {
+    if (kIsWeb) size = (size / 2).floor();
+
+    final PictureRecorder pictureRecorder = PictureRecorder();
+    final Canvas canvas = Canvas(pictureRecorder);
+    final Paint paint1 = Paint()..color = Colors.orange;
+    final Paint paint2 = Paint()..color = Colors.white;
+
+    canvas.drawCircle(Offset(size / 2, size / 2), size / 2.0, paint1);
+    canvas.drawCircle(Offset(size / 2, size / 2), size / 2.2, paint2);
+    canvas.drawCircle(Offset(size / 2, size / 2), size / 2.8, paint1);
+
+    if (text != null) {
+      TextPainter painter = TextPainter(textDirection: TextDirection.ltr);
+      painter.text = TextSpan(
+        text: text,
+        style: TextStyle(
+            fontSize: size / 3,
+            color: Colors.white,
+            fontWeight: FontWeight.normal),
+      );
+      painter.layout();
+      painter.paint(
+        canvas,
+        Offset(size / 2 - painter.width / 2, size / 2 - painter.height / 2),
+      );
+    }
+
+    final img = await pictureRecorder.endRecording().toImage(size, size);
+    final data = await img.toByteData(format: ImageByteFormat.png) as ByteData;
+
+    return BitmapDescriptor.fromBytes(data.buffer.asUint8List());
+  }
 }
